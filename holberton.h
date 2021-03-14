9b8a4b69fd1819380289960db296825adbaa9b62
merryta <merryta251@gmail.com>
2021-03-14 13:11:03 +0000

    added a header file

---
+++ holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#defina HOLBERTON_H
+
+#include <stdarg.h>
+typedef stract print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+#endif
