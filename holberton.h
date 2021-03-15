9b3bb20638148999ede1d29ca43fef666dd6d6d2
johnkatua <johnkatua99@gmail.com>
2021-03-15 11:05:10 +0000

    modified the struct declaration in the header file

--- holberton.h
+++ holberton.h
@@ -2,11 +2,11 @@
 #defina HOLBERTON_H
 
 #include <stdarg.h>
-typedef stract print
+typedef struct print
 {
 	char *t;
 	int (*f)(va_list);
-} print_t
+} print_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
@@ -14,4 +14,5 @@ int print_c(va_list c);
 int print_s(va_list s);
 int print_d(va_list d);
 int print_i(va_list i);
+
 #endif
