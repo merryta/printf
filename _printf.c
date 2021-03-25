db66d21f3102adc1c3067089f72b59db5b9a8c53
root <merryta251@gmail.com>
2021-03-25 15:00:19 +0000

    modified printf function

--- _printf.c
+++ _printf.c
@@ -6,7 +6,7 @@
  */
 int _printf(const char *format, ...)
 {
-	function_t identifier_f[] = {{'c', _printf_c}, {'s', _printf_s},
+	function_t identity_f[] = {{'c', _printf_c}, {'s', _printf_s},
 		{'i', print_number}, {'d', print_number}, {'b', _print_b},
 		{'o', _print_o}, {'u', _print_u}, {'x', _print_x},
 		{'X', _print_X}, {'\0', NULL}};
@@ -31,17 +31,17 @@ int _printf(const char *format, ...)
 			i++;
 		else
 		{
-			for (k = 0; identifier_f[k].id && format[i]; k++)
+			for (k = 0; identity_f[k].id && format[i]; k++)
 			{
-				if (identifier_f[k].id == format[flag])
+				if (identity_f[k].id == format[flag])
 				{
-					len_printf += identifier_f[k].f(flist);
+					len_printf += identity_f[k].f(flist);
 					i += 2;
 					break;
 				}
 			}
 		}
-		if (identifier_f[k].id == '\0' && format[i])
+		if (identity_f[k].id == '\0' && format[i])
 			_putchar(format[i++]), len_printf++;
 	}
 	va_end(flist);
